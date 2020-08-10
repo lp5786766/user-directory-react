eb9bcc33ac534a22faa853ab7117592b3e7185be
Luba Pecheneva <lp5786766@gmail.com>
2020-08-10 18:55:21 -0400

    trying not in srtict mode

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // {/* </React.StrictMode> */}
+  ,
   document.getElementById('root')
 );
 
