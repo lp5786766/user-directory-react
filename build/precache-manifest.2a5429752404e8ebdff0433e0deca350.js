d3a20a421734ba14308def854b8618620acfa424
Luba Pecheneva <lp5786766@gmail.com>
2020-08-11 16:52:35 -0400

    deploying

---
+++ build/precache-manifest.2a5429752404e8ebdff0433e0deca350.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be117733dc043687fb61eddacc1284f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b049099fa35d205dfe19",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "850cec9c02ec7d7a57a9",
+    "url": "/static/css/main.b6fa9942.chunk.css"
+  },
+  {
+    "revision": "b049099fa35d205dfe19",
+    "url": "/static/js/2.ffbd05cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ffbd05cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850cec9c02ec7d7a57a9",
+    "url": "/static/js/main.1c743338.chunk.js"
+  },
+  {
+    "revision": "5a658f61801743a1dc6d",
+    "url": "/static/js/runtime-main.794cdbbf.js"
+  }
+]);
